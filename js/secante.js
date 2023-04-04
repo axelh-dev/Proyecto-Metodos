a8414b41916636b46db0ccd768af92d387ff5047
Axel <aherreram22@miumg.edu.com>
2023-04-03 23:25:23 -0600

    terminado el metodo de newton y comienso del metodo de la secante

--- js/secante.js
+++ js/secante.js
@@ -1,15 +1,16 @@
 document
-  .getElementById("newton-form")
+  .getElementById("secante-form")
   .addEventListener("submit", function (event) {
     event.preventDefault();
 
     // Obtener los valores de los campos del formulario
     const equation = document.getElementById("equation").value;
+    const xo = parseFloat(document.getElementById("xo").value);
     const xi = parseFloat(document.getElementById("xi").value);
     const tolerance = parseFloat(document.getElementById("tolerance").value);
 
     /* Llamamos nuestro metodo y le pasamos los valores que vienen de los imputs */
-    const resultados = newton(equation, xi, tolerance);
+    let resultados = biseccion(equation, xo, xi, tolerance);
 
     // Aca limpiamos la tabla antes de mandar nuevos valores a ella
     const tabla = document.getElementById("tabla-resultados");
@@ -18,7 +19,6 @@ document
     for (let i = numFilas - 1; i > 0; i--) {
       tabla.deleteRow(i);
     }
-
     const tablaResultados = document.getElementById("tabla-resultados");
 
     // Agregamos una fila por cada resultado en el arreglo
@@ -36,65 +36,36 @@ document
     });
 
     // Definir la ecuacion a resolver utilizando math.js
-    function newton(funcion, xi, tolerance) {
+    function biseccion(funcion, xo, xi, tolerance) {
       let resultados = [];
+      let fxo = math.evaluate(funcion, { x: xo });
 
-      // Obtenemos la derivada de nuestra funcion devulve una funcion tipo node
-      let eqDer = math.derivative(funcion, "x");
-
-      // Convertimos el objeto Node a una cadena de texto
-      let eqDerStr = eqDer.toString();
-
-      // Remplazamos X en nuestra Ecuacion normal
       let fxi = math.evaluate(funcion, { x: xi });
 
-      // Remplazamos X en nuestra Ecuacion derivada
-      let fdxi = math.evaluate(eqDerStr, { x: xi });
-
-      // Calculamos recurrencia
+      /* Calculamos la recurrencia */
+      let xr = xi - (fxi * (xo - xi)) / (fxo - fxi);
+      /* Sustituimos recurrencia en nuestra ecuacion */
 
       let iteracion = 1;
       let toll = 0;
-
       /* creamos un ciclo que va a calcular cada iteracion y la va a guardar en un arreglo */
-      let recAnterior = 0;
-      let xr = xi - fxi / fdxi;
-      let fxr = math.evaluate(funcion, { x: xr });
-
+      let xrAnterior = 0;
       while (Math.abs(fxr) > tolerance) {
-        resultados.push([
-          iteracion,
-          xi.toFixed(6),
-          fxi.toFixed(6),
-          fdxi.toFixed(6),
-          xr.toFixed(6),
-          toll.toFixed(6),
-        ]); // guardamos los resultados
+        resultados.push([iteracion, xo, xi, fxo, fxi, xr, toll]); // guardamos los resultados
 
-        xi = xr;
+        xrAnterior = xr; // actualizamos xrAnterior con el valor actual de xr
 
-        recAnterior = xr; // actualizamos xrAnterior con el valor actual de xr
-
-        fxi = math.evaluate(funcion, { x: xi });
-
-        fdxi = math.evaluate(eqDerStr, { x: xi }); // Remplazamos X en nuestra Ecuacion derivada
-
-        xr = xi - fxi / fdxi; // Calculando la recurrencia por cada iteracion
+        xr = (a + b) / 2;
         fxr = math.evaluate(funcion, { x: xr });
+        fxr;
 
         iteracion++;
 
-        let diffXi = Math.abs(xr - recAnterior); // calculamos la diferencia entre xr actual y xr anterior
-        toll = diffXi; // actualizamos la tolerancia con la diferencia de xr
+        const diffXr = Math.abs(xr - xrAnterior); // calculamos la diferencia entre xr actual y xr anterior
+        toll = diffXr; // actualizamos la tolerancia con la diferencia de xr
+        toll;
       }
-      resultados.push([
-        iteracion,
-        xi.toFixed(6),
-        fxi.toFixed(6),
-        fdxi.toFixed(6),
-        xr.toFixed(6),
-        toll.toFixed(6),
-      ]); // guardamos el resultado final
+      resultados.push([iteracion, a, b, fa, fb, xr, fxr, toll]); // guardamos el resultado final
       return resultados;
     }
   });
